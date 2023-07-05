cf4aa2274abe603c329f4be53b612631c23ea91b
Anson0301 <126841266+Anson0301@users.noreply.github.com>
2023-07-05 16:46:58 +0800

    Add files via upload

---
+++ course/script.js
@@ -0,0 +1,10 @@
+function updateRating(value) {
+    var ratingValue = document.getElementById("rating-value");
+    ratingValue.textContent = value;
+}
+
+
+
+document.querySelector(".btn").addEventListener("click", function() {
+  alert("提交成功");
+});
